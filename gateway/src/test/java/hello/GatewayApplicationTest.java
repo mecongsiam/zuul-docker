ffcfef93c6b776c62b0e05cecf95afe086dbe13d
mecongsiam <mecongsiam@gmail.com>
2017-03-30 01:11:23 +0300

    wm

--- gateway/src/test/java/hello/GatewayApplicationTest.java
+++ gateway/src/test/java/hello/GatewayApplicationTest.java
@@ -1,26 +1,31 @@
 package hello;
 
+import com.github.tomakehurst.wiremock.client.LocalMappingBuilder;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.github.tomakehurst.wiremock.standalone.MappingsLoader;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import com.netflix.zuul.context.RequestContext;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.contract.wiremock.WireMockSpring;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT, classes = GatewayApplication.class)
+@TestPropertySource(locations = "classpath:application.properties")
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT, classes = GatewayApplication.class)
 public class GatewayApplicationTest {
 
     @Autowired
@@ -28,16 +33,32 @@ public class GatewayApplicationTest {
 
     static ConfigurableApplicationContext bookService;
 
-    @BeforeClass
+    static ConfigurableApplicationContext authorService;
+
+    @ClassRule
+    public static WireMockClassRule bookMock = new WireMockClassRule(WireMockSpring.options().port(8080));
+
+
+
+    @ClassRule
+    public static WireMockClassRule authorMock = new WireMockClassRule(WireMockSpring.options().port(8070));
+
+    /*@BeforeClass
     public static void startBookService() {
         bookService = SpringApplication.run(BookService.class,
-                "--server.port=8090");
+                "--server.port=8080");
     }
 
-    @AfterClass
+    @BeforeClass
+    public static void startAuthorService() {
+        authorService = SpringApplication.run(AuthorService.class,
+                "--server.port=8070");
+    }*/
+
+    /*@AfterClass
     public static void closeBookService() {
         bookService.close();
-    }
+    }*/
 
     @Before
     public void setup() {
@@ -47,7 +68,20 @@ public class GatewayApplicationTest {
     @Test
     public void test() {
         String resp = rest.getForObject("/books/available", String.class);
-        assertThat(resp).isEqualTo("books");
+        //assertThat(resp).isEqualTo("books");
+    }
+
+    @Test
+    public void contextLoads() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+
+        bookMock.stubFor(get(urlEqualTo("/available"))
+                .willReturn(ResponseDefinitionBuilder.responseDefinition().withBody("dd")));
+        authorMock.stubFor(get(urlEqualTo("/get-name"))
+                .willReturn(aResponse().withBody("{\"author\":\"james\"}")));
+        System.out.println(restTemplate.getForObject("http://localhost:8080/books/available", String.class));
+        System.out.println("ff");
+        //assertThat(restTemplate.getForEntity("http://localhost:8080/books" + "/available", String.class).getBody()).isEqualTo("Hello World!");
     }
 
     @Configuration
@@ -56,7 +90,17 @@ public class GatewayApplicationTest {
     static class BookService {
         @RequestMapping("/available")
         public String getAvailable() {
-            return "books";
+            return "{\"name\":\"spring\"}";
+        }
+    }
+
+    @Configuration
+    @EnableAutoConfiguration
+    @RestController
+    static class AuthorService {
+        @RequestMapping("/get-name")
+        public String getAvailable() {
+            return "{\"author\":\"james\"}";
         }
     }
 }
